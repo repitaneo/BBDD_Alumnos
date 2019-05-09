8bd4bae4cd440ba142d3bc21e7cbaefdaacb0f88
Marcos Fdez <marcosd@educastur.org>
2019-05-09 08:26:14 +0200

    Inicio del proyecto
    * READ - Ok

---
+++ src/com/marcos/go/Start.java
@@ -0,0 +1,16 @@
+package com.marcos.go;
+
+import com.marcos.modelo.AlumnosDataRead;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		AlumnosDataRead adr = new AlumnosDataRead();
+		adr.getAlumnos();
+		
+	}
+
+}
---
+++ src/com/marcos/modelo/AlumnosDataRead.java
@@ -0,0 +1,82 @@
+package com.marcos.modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.marcos.modelo.util.BDConect;
+
+public class AlumnosDataRead {
+
+	
+	
+	
+	public ArrayList<AlumnoBean> getAlumnos() {
+		
+		// preparo la conexion
+		Connection conexion = BDConect.getConexion(); 
+       	PreparedStatement stmt;
+	
+       	try {
+       		
+       		//  configuro una query SQL
+			stmt = conexion.prepareStatement("SELECT * FROM alumnos");
+			// ejecuto la query
+	       	ResultSet resultadoSQL = stmt.executeQuery();
+	       	
+	       	
+	       	// recorrer el resultado
+	    	while (resultadoSQL.next()) {
+	    		
+	    		System.out.print(resultadoSQL.getInt("idalumno")+" ");
+	    		System.out.print(resultadoSQL.getString("nombre")+" ");
+	    		System.out.println(resultadoSQL.getString("dnis"));
+	    	}
+       	}
+       	catch(Exception e) {
+       		
+       		e.printStackTrace();
+       	}
+		
+	}
+	
+	
+	
+/*		
+		
+		
+		
+       	
+       	// ejeculo la sentencia
+		try {
+			stmt = conexion.prepareStatement("SELECT * FROM cines");
+	       	ResultSet rs = stmt.executeQuery();
+	       	while (rs.next()) {
+	       		
+	       		CineBean cine = new CineBean();
+	       		cine.setIdCine(rs.getInt("idcines"));
+	       		cine.setNombre(rs.getString("nombre"));
+	       		cine.setResponsable(rs.getString("responsable"));
+	       		cine.setTelefono(rs.getString("telefono"));
+	       		cine.setDireccion(rs.getString("direccion"));
+	       		
+	       		
+	       		PeliculasData peliculaData = new PeliculasData();
+	       		peliculaData.getPeliculas(cine);
+	       		
+	       		devolver.add(cine);
+	       	} 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return devolver;
+	}
+
+*/	
+	
+	
+
+}
